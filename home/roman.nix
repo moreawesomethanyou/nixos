e6a3a7eeb0676df5f51264463d663d596482bd31
roman <rtokarskij111@gmail.com>
2026-08-25 19:36:22 +0300

    протестировал добавление своих конфиг файлов в home-manager, добавив конфиг cava

--- home/roman.nix
+++ home/roman.nix
@@ -152,5 +152,5 @@ in
 
   # Пакеты только для пользователя ставятся сюда; системные — в
   # environment.systemPackages в hosts/nixos/configuration.nix.
-  home.packages = [ ];
+  home.packages = [ eog ];
 }
